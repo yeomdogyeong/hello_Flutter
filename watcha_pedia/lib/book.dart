37d480f2b702f46530fef8c3df6f9173277dad33
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-02-14 01:47:16 +0900

    [feat]book 객체설정

---
+++ watcha_pedia/lib/book.dart
@@ -0,0 +1,15 @@
+class Book {
+  String id;
+  String title;
+  String subtitle;
+  String thumbnail;
+  String previewLink;
+
+  Book({
+    required this.id,
+    required this.title,
+    required this.subtitle,
+    required this.thumbnail,
+    required this.previewLink,
+  });
+}
