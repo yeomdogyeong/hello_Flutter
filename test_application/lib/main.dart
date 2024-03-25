cbc502badc9cb609bc04ca12eba02cb4e42d5efb
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-03-25 15:51:22 +0900

    [layout]mainAxisSize와 main,crossAlignment

--- test_application/lib/main.dart
+++ test_application/lib/main.dart
@@ -18,33 +18,42 @@ class customContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 300,
-        padding: EdgeInsets.fromLTRB(10, 12, 10, 12),
-        decoration: BoxDecoration(
-          color: Colors.red.shade100,
-          border: Border.all(
-            color: Colors.red,
-            width: 5,
-            style: BorderStyle.solid,
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                color: Colors.blue,
+                width: 100,
+                height: 100,
+                child: Text('1'),
+              ),
+              Container(
+                color: Colors.orange,
+                width: 100,
+                height: 100,
+                child: Text('1'),
+              ),
+              Container(
+                color: Colors.pink,
+                width: 100,
+                height: 100,
+                child: Text('1'),
+              ),
+            ],
           ),
-          borderRadius: BorderRadius.circular(100),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              offset: Offset(2, 2),
-              blurRadius: 20,
-            ),
-            BoxShadow(
-              color: Colors.orange.withOpacity(0.5),
-              offset: Offset(-2, -2),
-              blurRadius: 20,
-            )
-          ],
-        ),
-        child: Center(child: Text('im a body')),
+          Container(
+            height: 100,
+            width: 300,
+            child: Text('mango'),
+            color: Colors.yellowAccent,
+          ),
+        ],
       ),
     );
   }
