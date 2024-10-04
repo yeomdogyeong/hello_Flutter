78608435fe4a9d26046a046878c6ac67aeafb9af
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-04 18:58:26 +0900

    Innit: const 주석삭제 코드 추가

--- example927/flutter_application_1/lib/main.dart
+++ example927/flutter_application_1/lib/main.dart
@@ -1,16 +1,19 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     //가장 기본, 최상위 앱
-    const MaterialApp(
+    MaterialApp(
       //기본 앱을 쉽게 만들 수 있는 scaffold
       home: Scaffold(
+        appBar: AppBar(),
         body: SafeArea(
           child: Text(
             'ffffigmasssaaabbb!',
             style: TextStyle(
               color: Colors.black,
+              fontSize: 32,
             ),
           ),
         ),
