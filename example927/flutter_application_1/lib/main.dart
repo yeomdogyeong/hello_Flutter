21b71c3066e286ccce09c2356a2c28673841e537
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-04 22:54:06 +0900

    Feat: stateLessWidget으로 container 생성

--- example927/flutter_application_1/lib/main.dart
+++ example927/flutter_application_1/lib/main.dart
@@ -7,28 +7,48 @@ void main() {
     MaterialApp(
       //기본 앱을 쉽게 만들 수 있는 scaffold
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('This is App bar!'),
-          backgroundColor: Colors.lightBlue,
-          actions: [
-            IconButton(
-              icon: Icon(
-                Icons.home,
+          appBar: AppBar(
+            title: Text('This is App bar!'),
+            backgroundColor: Colors.lightBlue,
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.home,
+                ),
+                onPressed: () => print('Hello Mango'),
               ),
-              onPressed: () => print('Hello Mango'),
-            ),
-          ],
-        ),
-        body: SafeArea(
-          child: Text(
-            'ffffigmasssaaabbb!',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 32,
-            ),
+            ],
           ),
-        ),
-      ),
+          body: TestStateLess()
+          // SafeArea(
+          //   child: Text(
+          //     'ffffigmasssaaabbb!',
+          //     style: TextStyle(
+          //       color: Colors.black,
+          //       fontSize: 32,
+          //     ),
+          //   ),
+          // ),
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: () => print('hey!'),
+          //   child: Icon(Icons.bug_report),
+          // ),
+          ),
     ),
   );
+}
+
+class TestStateLess extends StatelessWidget {
+  const TestStateLess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //컨테이너는 기본적으로 child를 가질 수 있음
+      child: Center(child: Text('Hello Container')),
+      color: Colors.purple.shade200,
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
 }
