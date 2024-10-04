768240c30283b256ead198b3ebe6225d03a8e4e9
dogyeong <82639552+yeomdogyeong@users.noreply.github.com>
2024-10-04 19:34:32 +0900

    Add: icon 추가

--- example927/flutter_application_1/lib/main.dart
+++ example927/flutter_application_1/lib/main.dart
@@ -7,7 +7,18 @@ void main() {
     MaterialApp(
       //기본 앱을 쉽게 만들 수 있는 scaffold
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('This is App bar!'),
+          backgroundColor: Colors.lightBlue,
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.home,
+              ),
+              onPressed: () => print('Hello Mango'),
+            ),
+          ],
+        ),
         body: SafeArea(
           child: Text(
             'ffffigmasssaaabbb!',
